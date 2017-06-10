0d2be882f68af4a84f0aae02c661261bb0e59d35
Ben Zeise <ben.zeise@gmail.com>
2017-06-10 16:24:54 -0500

    further styling changes

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -9,57 +9,75 @@ myApp.controller( 'LifeGallery', function(){
   // init cards
   vm.cards = [];
 
+  // object constructor to make card objects
+  // add created objects
+  // WHY DIDN'T THIS WORK
+
+ // function card(url, desc, likes, showText) {
+ //   this.url = url;
+ //   this.desc = desc;
+ //   this.likes = likes;
+ //   this.showText = showText;
+ // }
+ //  var walkerPB = new card('pics/walkerPB.jpg','This summer, with Cassie in costume as Westley and Buttercup',0,false);
+
+  // Trying Dev's approach
   // make a temp object for each picture
-  var temp = {
+  var picInfo = {
     url: 'pics/walkerPB.jpg',
     description: 'This summer, with Cassie in costume as Westley and Buttercup',
     likes: 0,
     showText: false
   };
-  vm.cards.push( temp );
-  temp = {
+  vm.cards.push( picInfo );
+
+  picInfo = {
     url: 'pics/bballfam.jpg',
     description: 'With brother, brother-in-law, and Dad before a Twins game',
     likes: 0,
     showText: false
   };
-  vm.cards.push( temp );
-  temp = {
+  vm.cards.push( picInfo );
+
+  picInfo = {
     url: 'pics/con.jpg',
     description: 'With friends Sidney, Dusty, and James at Con 2015',
     likes: 0,
     showText: false
   };
-  vm.cards.push( temp );
-  temp = {
+  vm.cards.push( picInfo );
+
+  picInfo = {
     url: 'pics/minigolf.jpg',
     description: 'I got two holes-in-one that day.  Minigolf Master over here!',
     likes: 0,
     showText: false
   };
-  vm.cards.push( temp );
-  temp = {
+  vm.cards.push( picInfo );
+
+  picInfo = {
     url: 'pics/batb.jpg',
     description: 'Summer of 2006, Beauty and the Beast -- I\'m Cogsworth!',
     likes: 0,
     showText: false
   };
-  vm.cards.push( temp );
-  temp = {
+  vm.cards.push( picInfo );
+
+  picInfo = {
     url: 'pics/costumes.jpg',
     description: 'Oh man... 1993?  I\'m the cute Raphael.',
     likes: 0,
     showText: false
   };
-  vm.cards.push( temp );
+  vm.cards.push( picInfo );
 
   // toggle showing card or text by inverting showText value
   vm.toggleCard = function( index ){
     console.log( 'in toggleCard:', index );
-    vm.cards[ index ].showText = !vm.cards[ index ].showText;
+    vm.cards[index].showText = !vm.cards[index].showText;
   }; //end toggleCard
 
   vm.like = function( index ){
-    vm.cards[ index ].likes++;
+    vm.cards[index].likes++;
   }; //end like
 }); //end LifeGallery
